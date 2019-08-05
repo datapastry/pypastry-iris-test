2bf7802b211203e6a940e82f0b897fb7a8c0b141
Daoud Clarke <daoud.clarke@tesco.com>
2019-08-05 22:26:23 +0100

    Run evaluation

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from sklearn.metrics import f1_score
+from sklearn.metrics import f1_score, make_scorer
 from sklearn.model_selection import StratifiedKFold
 
 from pypastry import run_experiment
@@ -10,9 +10,9 @@ def run():
     candidates = pd.read_csv('../data/iris.csv')
     predictor = DecisionTreeClassifier()
     cross_validator = StratifiedKFold(n_splits=5)
-    metric = f1_score
+    scorer = make_scorer(f1_score, average='micro')
     label_column = 'species'
-    run_experiment(candidates, label_column, predictor, cross_validator, metric)
+    run_experiment(candidates, label_column, predictor, cross_validator, scorer)
 
 
 if __name__ == "__main__":
