6500558a529e83a47ee68ba7318692561d6d298f
Daoud Clarke <daoud.clarke@gmail.com>
2019-08-08 21:43:11 +0100

    Add results

--- pie.py
+++ pie.py
@@ -1,19 +1,16 @@
 import pandas as pd
+from pypastry import Experiment
 from sklearn.metrics import f1_score, make_scorer
 from sklearn.model_selection import StratifiedKFold
 
-from pypastry import run_experiment
 from sklearn.tree import DecisionTreeClassifier
 
 
-def run():
+def get_experiment():
     candidates = pd.read_csv('../data/iris.csv')
     predictor = DecisionTreeClassifier()
     cross_validator = StratifiedKFold(n_splits=5)
     scorer = make_scorer(f1_score, average='micro')
     label_column = 'species'
-    run_experiment(candidates, label_column, predictor, cross_validator, scorer)
+    return Experiment(candidates, label_column, predictor, cross_validator, scorer)
 
-
-if __name__ == "__main__":
-    run()
